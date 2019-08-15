4b2ec88e5b18903e9789b00c31fad9a26ca35124
nnijat <nn.aca2019@gmail.com>
2019-08-15 00:55:17 -0500

    completed Part1, The components section

---
+++ public/js/components/App.js
@@ -0,0 +1,29 @@
+function App(props) {
+    return (
+        <div class="App">
+            {/*</div><!--<Header>-->*/}
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-3">
+                        <p class="lead">Shop Name</p>
+                        <div class="list-group">
+                            <a href="#" class="list-group-item">Category 1</a>
+                            <a href="#" class="list-group-item">Category 2</a>
+                            <a href="#" class="list-group-item">Category 3</a>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        {/*<!--<Carousel>-->*/}
+                        <div class="row">
+                            {/*<!--<ProductDetail>-->*/}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <hr />
+                {/*<!--</Footer>-->*/}
+            </div>
+        </div>
+    )
+}
